e10b152d47501edd635d54bd40a78febbd3ab7a9
neoneoneo123 <gouxrunner@gmail.com>
2024-07-16 19:02:17 +0900

    feat: mypage 구조 작업 - add: mypage fragment 활용 string 추가 - add: mypage fragment에 필요한 image source 추가 - add: mypage fragment 추가
    issue:#3

---
+++ app/src/main/java/com/sw/wordgarden/presentation/ui/mypage/MypageFragment.kt
@@ -0,0 +1,23 @@
+package com.sw.wordgarden.presentation.ui.mypage
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sw.wordgarden.R
+
+class MypageFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_mypage, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
