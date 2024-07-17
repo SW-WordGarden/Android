8cd4f77f747ca1452f9c4047d0d12e0dcd4f805e
neoneoneo123 <gouxrunner@gmail.com>
2024-07-17 16:30:47 +0900

    add: my page 하위 화면 생성
    issue:#3

---
+++ app/src/main/java/com/sw/wordgarden/presentation/ui/mypage/QuizzesFragment.kt
@@ -0,0 +1,18 @@
+package com.sw.wordgarden.presentation.ui.mypage
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sw.wordgarden.R
+
+class QuizzesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_quizzes, container, false)
+    }
+}
