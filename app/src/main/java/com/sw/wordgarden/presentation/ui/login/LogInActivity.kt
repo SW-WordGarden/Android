0f9e20755fbc00daf37954ac802b2cf50227d42b
neoneoneo123 <gouxrunner@gmail.com>
2024-07-20 13:48:44 +0900

    design: 로그인, 온보딩 화면 xml 구현
    issue:#4

--- app/src/main/java/com/sw/wordgarden/presentation/ui/login/LogInActivity.kt
+++ app/src/main/java/com/sw/wordgarden/presentation/ui/login/LogInActivity.kt
@@ -6,16 +6,34 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.sw.wordgarden.R
+import com.sw.wordgarden.databinding.ActivityLoginBinding
 
 class LogInActivity : AppCompatActivity() {
+
+    private val binding: ActivityLoginBinding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        uiSetting()
+        setFragment(savedInstanceState)
+    }
+
+    private fun uiSetting() {
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.clLoginMain) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun setFragment(bundle: Bundle?) {
+        if (bundle == null){ supportFragmentManager.beginTransaction()
+            .replace(R.id.cl_login_main, LoginFragment())
+            .commit()}
     }
 }
